e77b207fb479a5c3bcf073e5e1ddc965cb9f056c
Navnath parte <navnath.parte@codeb.online>
2022-10-27 15:10:00 +0530

    login api changes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,44 +1,40 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { loginDTO } from './dto/login.dto';
 import  * as fs from "fs"
 import * as jose from "node-jose"
 
 @Controller('auth')
 export class AuthController {
-    @Post()
-    async login(@Body() body: loginDTO) {
-        try {
-            const {name, id} = body;
+  @Get()
+  async login(@Query('name') name?: string, @Query('id') id?: string) {
+    try {
+      // if (!name || !id) {
+      //     throw new Error("Invalid Request")
+      // }
 
-            // if (!name || !id) {
-            //     throw new Error("Invalid Request")
-            // }
+      const JWKeys = fs.readFileSync('keys.json');
 
-            const JWKeys = fs.readFileSync("keys.json");
+      console.log(JWKeys);
 
-            console.log(JWKeys)
+      const keyStore = await jose.JWK.asKeyStore(JWKeys.toString());
+      // console.log('keyStore', keyStore)
+      const [key] = keyStore.all({ use: 'sig' });
 
-            const keyStore = await jose.JWK.asKeyStore(JWKeys.toString());
-            // console.log('keyStore', keyStore)
-            const [key] = keyStore.all({ use: "sig" });
+      const opt = { compact: true, jwk: key, fields: { typ: 'jwt' } };
 
-            const opt = { compact: true, jwk: key, fields: { typ: "jwt" } };
+      const payload = JSON.stringify({
+        exp: '1d',
+        sub: 'dummy-app',
+        name,
+        uid: id,
+        iat: Math.floor(Date.now() / 1000),
+      });
 
-            const payload = JSON.stringify({
-                exp: "1d",
-                sub: "dummy-app",
-                name,
-                uid: id,
-                iat: Math.floor(Date.now() / 1000),
-            });
-
-            const token = await jose.JWS.createSign(opt, key).update(payload).final();
-            console.log('token', token)
-            
-            return token;
-
-        } catch (error) {
-            console.log("error",error);
-        }
+        const token = await jose.JWS.createSign(opt, key).update(payload).final();
+        
+      return token;
+    } catch (error) {
+      console.log('error', error);
     }
+  }
 }
